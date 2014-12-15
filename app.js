82ee7080e0a6a93122c4f39318538b4bf1d09d9b
Jan Jaap de Groot <jjedegroot@gmail.com>
2014-12-15 17:10:30 +1100

    Email sending

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(process.env.PORT || 8080);
+/** HOSTING */
+var express = require('express'); // Express
+var app = express();
+app.use(express.static(__dirname + '/'));
+
+/** EMAIL */
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'sustainabilitycircles2015@gmail.com',
+        pass: '$ustainable'
+    }
+});
+
+app.get('/', function (req, res) {
+    res.render('index.html');
+});
+
+app.post('/mail', function (req, res) {
+	console.log(req);
+
+    // setup e-mail data with unicode symbols
+	var mailOptions = {
+	    from: 'Circles of Sustainability <sustainabilitycircles2015@gmail.com>', // sender address
+	    to: req.query.receivers, // list of receivers
+	    subject: 'Circles of Sustainability report', // Subject line
+	    text: 'Plain text', // plaintext body
+	    html: req.query.html // html body
+	};
+
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, function(error, info){
+	    if(error){
+	    	console.log('Error sending email :(');
+	        console.log(error);
+	        res.send(error);
+	    }else{
+	        console.log('Message sent successfully :)');
+	        console.log(info);
+	        res.send(info);
+	    }
+	});
+});
+
+app.listen(process.env.PORT || 8080);
+console.log('Your app is now running at: http://127.0.0.1:8080/');
--- js/workflow.js
+++ js/workflow.js
@@ -1,7 +1,20 @@
 /** Generates and shows the result */
 function _generate() {
+	// Construct URL
+	var receivers = "sustainabilitycircles2015@gmail.com";
 	var html = $("#rightcol").html();
+	var url = "mail?receivers=" + encodeURI(receivers) + "&html=" + encodeURI(html);
+	console.log("POST URL: " + url);
 
+	// Post the mail
+	$.post(url, function() {
+		alert( "success" );
+	}).fail(function() {
+		alert("failed");
+	});
+
+
+	// Show code
 	$("#code").text(html);
 	$("html, body").animate({ scrollTop: $(document).height() });
 	$("#result").show(500);
